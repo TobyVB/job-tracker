fb5151e49b087d401d6a2529df2c53dc1a6a24b5
TobyVB <98196156+TobyVB@users.noreply.github.com>
2023-04-18 17:41:31 +0800

    added new job, and created main structure of calendar

--- data.js
+++ data.js
@@ -1,36 +1,77 @@
 const jobs = [
   {
-    organization: "Aquent",
+    org: "Aquent",
     link: "https://aquent.com/",
     position: "Front End React Developer",
-    timeOfApplication: "1681792644852",
+    time: 1681792644852,
+    catered: "no",
     response: false,
     firstInterview: false,
     secondInterview: false,
     thirdInterview: false,
     hired: false,
+    middleMan: "linkedin",
+    message: "",
+  },
+  {
+    org: "Lancesoft",
+    link: "",
+    position: "Web Developer",
+    time: 1681806880138,
+    catered: "somewhat",
+    response: false,
+    firstInterview: false,
+    secondInterview: false,
+    thirdInterview: false,
+    hired: false,
+    middleMan: "linkedin",
+    message: `Greetings, Lancesoft, my name is Toby Van Baast and I'm a Web Developer from Austin. Your add on LinkedIn caught my interest.
+Currently, I have been visiting family in Perth, Australia. I'll return to Austin, April 30th with an American phone number.
+
+I have had a singular focus on learning Web Development and building projects the past 2 years, and around 5 years on and off. Some of that experience is with Python and Django; however, most of my experience is Javascript and React.
+
+Looking at your website really resonated with me, the design language just makes sense and the message is clear and practical. 
+I also enjoyed the 4 Ps.
+Here are mine.
+Purpose:
+My purpose is to learn and put into practice what should be prioritized in order to be effective and resourceful.
+People:
+I strive to practice appreciation, and to realize value in others and the environment.
+Performance:
+I greatly appreciate quick and frequent feedback of my actions. This serves as great help in recognizing patterns and developing an awareness of effective actions.
+Partnerships:
+Maintaining relationships through the seasons, improves trust, loyalty, and sense of familiarity. Community makes us all accountable.
+
+I would be absolutely pleased to work with you.
+Here is my portfolio: https://portfolio-90faf.web.app/`,
   },
   //   {
-  //     title: "",
+  //     org: "",
   //     link: "",
   //     position: "",
-  //     timeOfApplication: "",
+  //     time: ,
+  //     catered: false,
   //     response: false,
   //     firstInterview: false,
   //     secondInterview: false,
   //     thirdInterview: false,
   //     hired: false,
+  //     middleMan: false,
+  //     message:""
   //   },
   //   {
-  //     title: "",
+  //     org: "",
   //     link: "",
   //     position: "",
-  //     timeOfApplication: "",
+  //     time: ,
+  //     catered: false,
   //     response: false,
   //     firstInterview: false,
   //     secondInterview: false,
   //     thirdInterview: false,
   //     hired: false,
+  //     middleMan: false,
+  //     message:""
   //   },
 ];
 export default { jobs };
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import jobs from "../data.js";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  function getTime(date) {
+    const time = new Date(date);
+    return time.toLocaleDateString();
+  }
+
+  function newArr() {
+    const newArr = [];
+    jobs.jobs.map((job, idx) => {
+      !newArr.includes(getTime(job.time)) && newArr.push(getTime(job.time));
+    });
+    return newArr;
+  }
+  console.log(newArr());
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {newArr().map((date, idx) => {
+        return (
+          <div key={idx} style={{ border: "1px solid white" }}>
+            <h2 style={{ fontSize: "2rem" }}>{date}</h2>
+            <div>
+              {jobs.jobs.map((job, idx) => {
+                getTime(job.time) === getTime(date.time);
+                return (
+                  <div key={idx} style={{ textAlign: "left", padding: "2em" }}>
+                    <h2 style={{ fontSize: "1.5rem" }}>{job.org}</h2>
+                    <h3>{job.position}</h3>
+                    <p style={{ fontWeight: "bold" }}>{job.middleMan}</p>
+                    <p>catered: {job.catered}</p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
