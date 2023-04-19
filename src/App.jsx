7cade2f352f06e6e72a7a00891196d6c4cf24eb2
TobyVB <98196156+TobyVB@users.noreply.github.com>
2023-04-19 23:21:08 +0800

    add 2 new jobs

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     });
     return newArr;
   }
-  console.log(newArr());
+  console.log(Date.now());
 
   return (
     <div className="App">
@@ -23,16 +23,41 @@ function App() {
         return (
           <div key={idx} style={{ border: "1px solid white" }}>
             <h2 style={{ fontSize: "2rem" }}>{date}</h2>
-            <div>
+            <div style={{ display: "flex" }}>
               {jobs.jobs.map((job, idx) => {
-                getTime(job.time) === getTime(date.time);
                 return (
-                  <div key={idx} style={{ textAlign: "left", padding: "2em" }}>
-                    <h2 style={{ fontSize: "1.5rem" }}>{job.org}</h2>
-                    <h3>{job.position}</h3>
-                    <p style={{ fontWeight: "bold" }}>{job.middleMan}</p>
-                    <p>catered: {job.catered}</p>
-                  </div>
+                  <>
+                    {getTime(job.time) === date && (
+                      <div
+                        key={idx}
+                        style={
+                          job.available
+                            ? {
+                                background: "rgba(0,255,0,.05)",
+                                padding: "2em",
+                                textAlign: "left",
+                                border: "1px solid green",
+                                width: "20vw",
+                              }
+                            : {
+                                background: "rgba(255,0,0,.1)",
+                                padding: "2em",
+                                textAlign: "left",
+                                border: "1px solid red",
+                                width: "20vw",
+                              }
+                        }
+                      >
+                        <h2 style={{ fontSize: "1.5rem" }}>{job.org}</h2>
+                        <h3>{job.position}</h3>
+                        <p style={{ fontWeight: "bold" }}>{job.middleMan}</p>
+                        <p>catered: {job.catered}</p>
+                        <a href={job.link} target="_blank">
+                          {job.link}
+                        </a>
+                      </div>
+                    )}
+                  </>
                 );
               })}
             </div>
