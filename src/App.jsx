7a5e65e64a31d3704a16c36a04b66d5422b49aa1
TobyVB <98196156+TobyVB@users.noreply.github.com>
2023-04-20 21:56:11 +0800

    add 3 new jobs

--- src/App.jsx
+++ src/App.jsx
@@ -19,51 +19,53 @@ function App() {
 
   return (
     <div className="App">
-      {newArr().map((date, idx) => {
-        return (
-          <div key={idx} style={{ border: "1px solid white" }}>
-            <h2 style={{ fontSize: "2rem" }}>{date}</h2>
-            <div style={{ display: "flex" }}>
-              {jobs.jobs.map((job, idx) => {
-                return (
-                  <>
-                    {getTime(job.time) === date && (
-                      <div
-                        key={idx}
-                        style={
-                          job.available
-                            ? {
-                                background: "rgba(0,255,0,.05)",
-                                padding: "2em",
-                                textAlign: "left",
-                                border: "1px solid green",
-                                width: "20vw",
-                              }
-                            : {
-                                background: "rgba(255,0,0,.1)",
-                                padding: "2em",
-                                textAlign: "left",
-                                border: "1px solid red",
-                                width: "20vw",
-                              }
-                        }
-                      >
-                        <h2 style={{ fontSize: "1.5rem" }}>{job.org}</h2>
-                        <h3>{job.position}</h3>
-                        <p style={{ fontWeight: "bold" }}>{job.middleMan}</p>
-                        <p>catered: {job.catered}</p>
-                        <a href={job.link} target="_blank">
-                          {job.link}
-                        </a>
-                      </div>
-                    )}
-                  </>
-                );
-              })}
+      {newArr()
+        .reverse()
+        .map((date, idx) => {
+          return (
+            <div key={idx} style={{ border: "1px solid white" }}>
+              <h2 style={{ fontSize: "2rem" }}>{date}</h2>
+              <div style={{ display: "flex" }}>
+                {jobs.jobs.map((job, idx) => {
+                  return (
+                    <>
+                      {getTime(job.time) === date && (
+                        <div
+                          key={idx}
+                          style={
+                            job.available
+                              ? {
+                                  background: "rgba(0,255,0,.05)",
+                                  padding: "2em",
+                                  textAlign: "left",
+                                  border: "1px solid green",
+                                  width: "20vw",
+                                }
+                              : {
+                                  background: "rgba(255,0,0,.1)",
+                                  padding: "2em",
+                                  textAlign: "left",
+                                  border: "1px solid red",
+                                  width: "20vw",
+                                }
+                          }
+                        >
+                          <h2 style={{ fontSize: "1.5rem" }}>{job.org}</h2>
+                          <h3>{job.position}</h3>
+                          <p style={{ fontWeight: "bold" }}>{job.middleMan}</p>
+                          <p>catered: {job.catered}</p>
+                          <a href={job.link} target="_blank">
+                            {job.link}
+                          </a>
+                        </div>
+                      )}
+                    </>
+                  );
+                })}
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </div>
   );
 }
